3ad6172ed4a2e861a368c6b357d2758dc4f3fc21
Antonio <anzekic@vsite.hr>
2015-12-03 20:01:40 +0100

    ParametarRefTip

--- ParametarRef/ParametarRefTip.cs
+++ ParametarRef/ParametarRefTip.cs
@@ -19,8 +19,6 @@ namespace Vsite.CSharp
 
         static void Main(string[] args)
         {
-            // TODO: Pokrenuti program, ispis prekopirati u datoteku ParametarRefTip.txt te u njoj dopisati obrazloženje rezultata
-
             MojaKlasa mk = new MojaKlasa();
             Console.WriteLine("Prije metode Uvećaj: {0}", mk.N);
             Uvećaj(mk);
